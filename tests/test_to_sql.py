3b7c283950f4c8ed82b44a421f86263c0ac5c456
Charlie Angela Mehlenbeck <charlie_inventor2003@yahoo.com>
2023-10-03 21:07:14 -0400

    + test for list

--- tests/test_to_sql.py
+++ tests/test_to_sql.py
@@ -6,6 +6,8 @@ from datetime import datetime
 
 from Attributes.to_sql import to_sql, DefaultBase # Your specific import
 
+from typing import List
+
 class TestSQLAlchemyDataClasses(unittest.TestCase):
 	def __create_db__(self):
 		self.engine = create_engine('sqlite:///:memory:', echo=True)
@@ -61,6 +63,68 @@ class TestSQLAlchemyDataClasses(unittest.TestCase):
 		self.assertEqual(obj2.field3.field2, obj.field2)
 		self.assertEqual(obj2.field3.field3, obj.field3)
 		self.assertEqual(obj2.field3.field4, obj.field4)
+	
+	def test_lists(self):
+		# Create test database
+		@to_sql
+		@dataclass
+		class TestClass:
+			__primary_key_name__ = "field1"
+			field1: int = 1
+			field2: str = "test"
+		
+		@to_sql
+		@dataclass
+		class TestClass2:
+			__primary_key_name__ = "field1"
+			field1: int = 1
+			list1: List[TestClass] = field(default_factory=TestClass)
+		
+		self.__create_db__()
+		
+		# Create test data
+		test1 = TestClass(field1=1, field2="a thing")
+		test2 = TestClass(field1=2, field2="another thing")
+		test3 = TestClass(field1=3, field2="a third thing")
+		test_list = TestClass2()
+		test_list.list1.append(test1)
+		test_list.list1.append(test2)
+		test_list.list1.append(test3)
+		
+		# Add test data to database
+		self.session.add(test1.to_schema())
+		self.session.add(test2.to_schema())
+		self.session.commit()
+		
+		# Query test data from database
+		obj = self.session.query(TestClass.__SQL_Schema_Class__).all()
+		obj2 = self.session.query(TestClass2.__SQL_Schema_Class__).first()
+		
+		# Assert test data is equal
+		# Check test 1, 2, and 3
+		self.assertEqual(obj[0].field1, 1)
+		self.assertEqual(obj[0].field2, "a thing")
+		
+		self.assertEqual(obj[1].field1, 2)
+		self.assertEqual(obj[1].field2, "another thing")
+		
+		self.assertEqual(obj[2].field1, 3)
+		self.assertEqual(obj[2].field2, "a third thing")
+		
+		# Check test list
+		self.assertEqual(obj2.field1, 1)
+		self.assertEqual(len(obj2.list1), 3)
+		
+		self.assertEqual(obj2.list1[0].field1, obj[0].field1)
+		self.assertEqual(obj2.list1[0].field2, obj[0].field2)
+		
+		self.assertEqual(obj2.list1[1].field1, obj[1].field1)
+		self.assertEqual(obj2.list1[1].field2, obj[1].field2)
+		
+		self.assertEqual(obj2.list1[2].field1, obj[2].field1)
+		self.assertEqual(obj2.list1[2].field2, obj[2].field2)
+		
+		
 		
 if __name__ == '__main__':
 	unittest.main()
