b7486f747ad273a32d95033be1a81e1bd6928d6b
Charlie Angela Mehlenbeck <charlie_inventor2003@yahoo.com>
2023-12-03 12:17:09 -0500

    expanding example

--- ClassyFlaskDB/Flaskify/example/example_client.py
+++ ClassyFlaskDB/Flaskify/example/example_client.py
@@ -8,4 +8,14 @@ flaskify_client = FlaskifyClientDecorator(type_serializer_mapping, base_url="htt
 ClientifiedMyService = flaskify_client()(MyService)
 ClientifiedAnotherService = flaskify_client("another")(AnotherService)
 
+from pydub import AudioSegment
+a_s = AudioSegment.from_file("hello.mp3", format="mp3")
+print(ClientifiedMyService.process_audio("hello", a_s))
+print(ClientifiedMyService.reverse_text("hello"))
+print(ClientifiedMyService.text_length("hello"))
+print(ClientifiedMyService.concatenate_texts("hello", "world"))
+
 print(ClientifiedAnotherService.add_numbers(1, 2))
+print(ClientifiedAnotherService.multiply_numbers(3, 4))
+print(ClientifiedAnotherService.upper_case_text("hello"))
+print(ClientifiedAnotherService.repeat_text("hello", 3))
--- ClassyFlaskDB/Flaskify/example/example_services.py
+++ ClassyFlaskDB/Flaskify/example/example_services.py
@@ -3,20 +3,24 @@ from ClassyFlaskDB.Flaskify.Route import Route
 
 class MyService:
     @Route(path='/process_audio', methods=['POST'])
-    def process_audio(self, text: str, audio: AudioSegment) -> str:
+    @staticmethod
+    def process_audio(text: str, audio: AudioSegment) -> str:
         # Dummy implementation
         return f"Processed text: {text} and audio length: {len(audio)} ms"
 
     @Route()
-    def reverse_text(self, text: str) -> str:
+    @staticmethod
+    def reverse_text(text: str) -> str:
         return text[::-1]
 
     @Route("/text_length_______blaaaah")
-    def text_length(self, text: str) -> int:
+    @staticmethod
+    def text_length(text: str) -> int:
         return len(text)
 
     @Route("/concatenate_texts")
-    def concatenate_texts(self, text1: str, text2: str) -> str:
+    @staticmethod
+    def concatenate_texts(text1: str, text2: str) -> str:
         return text1 + text2
 
 class AnotherService:
@@ -26,14 +30,17 @@ class AnotherService:
         return num1 + num2
 
     @Route()
-    def multiply_numbers(self, num1: int, num2: int) -> int:
+    @staticmethod
+    def multiply_numbers(num1: int, num2: int) -> int:
         return num1 * num2
 
     @Route()
-    def upper_case_text(self, text: str) -> str:
+    @staticmethod
+    def upper_case_text(text: str) -> str:
         return text.upper()
 
     @Route()
-    def repeat_text(self, text: str, times: int) -> str:
+    @staticmethod
+    def repeat_text(text: str, times: int) -> str:
         return text * times
 print("Hello World")
