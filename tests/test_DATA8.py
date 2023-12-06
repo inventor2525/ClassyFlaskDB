52bd459e2f354c09e8f5025755506dfcb124540d
Charlie Angela Mehlenbeck <charlie_inventor2003@yahoo.com>
2023-12-06 15:14:08 -0500

    + test with data classes inside the test case

---
+++ tests/test_DATA8.py
@@ -0,0 +1,62 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from datetime import datetime
+from ClassyFlaskDB.DATA import DATADecorator
+from dataclasses import field
+from typing import List
+import unittest
+
+class DATA_Decorator7(unittest.TestCase):
+	def test_function_nested_classes(self):
+		DATA = DATADecorator() #Re-define the DATA decorator since having only 1 accross all tests causes issues
+
+		# Define the data classes
+		@DATA
+		class Foe:
+			name: str
+			strength: int
+			bar: "Bar" = None
+
+		@DATA
+		class Bar:
+			name: str
+			location: str
+			foe: "Foe" = None
+
+		# Create an in-memory SQLite database
+		engine = create_engine('sqlite:///:memory:')
+
+		# Finalize the mapping
+		DATA.finalize(engine)
+
+		# Create a session
+		Session = sessionmaker(bind=engine)
+		session = Session()
+
+		foe = Foe(name="Dragon1", strength=100)
+		bar = Bar(name="Dragon's Lair", location="Mountain", foe=foe)
+
+		foe.bar = bar
+
+		# Insert into database
+		session.add(bar)
+		session.commit()
+
+		# Query from database
+		queried_bar = session.query(Bar).filter_by(name="Dragon's Lair").first()
+
+		# Validate
+		self.assertEqual(queried_bar.name, bar.name)
+		self.assertEqual(queried_bar.location, bar.location)
+		
+		self.assertEqual(queried_bar.foe.name, bar.foe.name)
+		self.assertEqual(queried_bar.foe.strength, bar.foe.strength)
+
+		self.assertEqual(queried_bar.foe.bar.name, bar.name)
+		self.assertEqual(queried_bar.foe.bar.location, bar.location)
+		self.assertEqual(queried_bar.foe.bar.uuid, bar.uuid)
+
+		session.close()
+
+if __name__ == '__main__':
+	unittest.main()
