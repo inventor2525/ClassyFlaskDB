74795c3ddd555308dea1691a4289e783d50ccdae
Charlie Angela Mehlenbeck <charlie_inventor2003@yahoo.com>
2023-12-28 14:40:36 -0500

    + print function

--- ClassyFlaskDB/DATA/__init__.py
+++ ClassyFlaskDB/DATA/__init__.py
@@ -3,3 +3,8 @@ from dataclasses import field
 
 from .DATADecorator import DATADecorator, ID_Type
 from .DATAEngine import DATAEngine
+
+def print_DATA_json(json_data:dict) -> None:
+	from ClassyFlaskDB.Flaskify.serialization import FlaskifyJSONEncoder
+	import json
+	print(json.dumps(json_data, indent=4, sort_keys=True, cls=FlaskifyJSONEncoder))
