30741c57d2fd2e60a9bbc14a4d035f3e70374d93
Charlie Angela Mehlenbeck <charlie_inventor2003@yahoo.com>
2024-07-11 16:53:03 -0400

    + circular ref test

--- tests/newDATADecorator_tests.py
+++ tests/newDATADecorator_tests.py
@@ -34,6 +34,48 @@ class newDATADecorator_tests(unittest.TestCase):
 		self.assertEqual(queried_bar.location, bar.location)
 		self.assertEqual(queried_bar.foe.name, foe.name)
 		self.assertEqual(queried_bar.foe.strength, foe.strength)
+	
+	def test_relationship_with_circular_ref(self):
+		DATA = DATADecorator()
+
+		# Define the data classes
+		@DATA
+		@dataclass
+		class Foe:
+			name: str
+			strength: int
+			bar: "Bar" = None
+
+		@DATA
+		@dataclass
+		class Bar:
+			name: str
+			location: str
+			foe: "Foe" = None
+			
+		data_engine = SQLAlchemyStorageEngine("sqlite:///:memory:", transcoder_collection, DATA)
+
+		foe = Foe(name="Dragon1", strength=100)
+		bar = Bar(name="Dragon's Lair", location="Mountain", foe=foe)
+
+		foe.bar = bar
+
+		# Insert into database
+		data_engine.merge(bar)
+
+		# Query from database
+		queried_bar = data_engine.query(Bar).filter_by_id(bar.get_primary_key())
+
+		# Validate
+		self.assertEqual(queried_bar.name, bar.name)
+		self.assertEqual(queried_bar.location, bar.location)
+		
+		self.assertEqual(queried_bar.foe.name, bar.foe.name)
+		self.assertEqual(queried_bar.foe.strength, bar.foe.strength)
+
+		self.assertEqual(queried_bar.foe.bar.name, bar.name)
+		self.assertEqual(queried_bar.foe.bar.location, bar.location)
+		self.assertEqual(queried_bar.foe.bar.auto_id, bar.auto_id)
 		
 if __name__ == '__main__':
 	unittest.main()
