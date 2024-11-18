4098239534d43ed606c665b556183a51a7ece456
Charlie Angela Mehlenbeck <charlie_inventor2003@yahoo.com>
2024-11-18 18:37:56 -0500

    fix flaskify tests (can't handle direct list return yet)

--- tests/Flaskify/model.py
+++ tests/Flaskify/model.py
@@ -17,6 +17,11 @@ class StoredMessage:
     message: Message
     response: str
 
+@DATA
+@dataclass
+class msg_list:
+    messages: List[StoredMessage]
+
 # Service objects
 FLASKIFY = FlaskifyDecorator(DATA)
 
@@ -33,8 +38,8 @@ class ChatHistory:
         return stored
     
     @FLASKIFY.route("/get_all")
-    def get_messages(self) -> List[StoredMessage]:
-        return self.messages
+    def get_messages(self) -> msg_list:
+        return msg_list(self.messages)
     
     @FLASKIFY.route("/clear")
     def clear(self) -> None:
